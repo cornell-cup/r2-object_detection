5a7177c75218b09cb4e602782d0f74eabab522aa
tginsbu <tginsbu@aol.com>
2019-02-22 12:57:50 -0500

    edited whitespace and encoding

--- piimages.py
+++ piimages.py
@@ -3,13 +3,15 @@ import numpy as np
 from PIL import Image
 import picamera
 import time
-
-
-def main():	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)						s.connect(('192.168.4.27',portnum))
+ip = sys.argv[1]
+portnum = sys.argv[2]
+def main():	
+	s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	s.connect(ip,portnum)
 	with picamera.PiCamera as camera():
 		camera.capture("image.png")
-	file = open("image.png", 'rb')
-	bytes = file.read()
+	img = open("image.png", 'rb')
+	bytes = img.read()
 	size = len(bytes)
 	s.send(size.encode())
 	s.sent(bytes.encode())
