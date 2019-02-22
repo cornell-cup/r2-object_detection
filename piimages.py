efa6cce87182e18875afcb0abbf77321bdf7351c
tginsbu <tginsbu@aol.com>
2019-02-22 13:05:45 -0500

    edited stuff

--- piimages.py
+++ piimages.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 import picamera
 import time
+import cv2
 ip = sys.argv[1]
 portnum = sys.argv[2]
 def main():	
@@ -10,11 +11,23 @@ def main():
 	s.connect(ip,portnum)
 	with picamera.PiCamera as camera():
 		camera.capture("image.png")
-	img = open("image.png", 'rb')
-	bytes = img.read()
-	size = len(bytes)
-	s.send(size.encode())
-	s.sent(bytes.encode())
+	frame = cv2.imread("image.png")
+	frameBytes = frame.tobytes()
+	byteLength = len(bytes)
+	shape0 = frame.shape[0]
+	shape1 = frame.shape[1]
+	s.send(str(byteLength).encode())
+	s.send(str(shape0).encode())
+	s.send(str(shape1).encode())
+	s.close()
+	try:	
+		s1=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		s1.connect(ip,portnum) 
+	except:
+		print("cannot reconnect")
+	print("connected")
+	s1.send(frameBytes)
+	s1.close()
 	time.sleep(.001)
 
 while true
