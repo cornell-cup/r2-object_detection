e7a038c5c444e096b8df467313aeb55c84559bbe
lhtlinda <18701770814@yeah.net>
2019-03-02 16:44:27 -0500

    server

--- new-server.py
+++ new-server.py
@@ -1,7 +1,8 @@
 import socket,os,sys
 import numpy as np
 from PIL import Image
-#from test import infer_and_match
+
+from test import infer_and_match
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((socket.gethostbyname(socket.gethostname()), int(sys.argv[1])))
 print(socket.gethostbyname(socket.gethostname()))
@@ -14,18 +15,29 @@ while 1:
     (clientsocket, address) = server_socket.accept()
     #now do something with the clientsocket
     #in this case, we'll pretend this is a threaded server
-    
+    print("connected to client")    
     while True:
         size = clientsocket.recv(1024)
         #print(size)
-        i=int(size)
+        print("got size")
+        
+        i=str(size)
+        print(i)
 
-        shape0=clientsocket.recv(1024)
+        
+        
+        s=str(i)[2:-1]
+        s=int(s)
+        shape1=1920
+        shape0=1080
+        shape2=3
         #print(shape0)
-        s=str(shape0)
-        s=s[2:]
-        s0=int(s[:3])
-        s1=int(s[3:6])
+        
+        #s=str(shape0)
+        #print(s)
+        #s=s[2:]
+        #s0=int(s[:3])
+        #s1=int(s[3:6])
         #shape1=clientsocket.recv(1024)
         #print(shape1)
         #s1=int(shape1)
@@ -33,9 +45,9 @@ while 1:
         #print(s1)
         #buf=clientsocket.recv(i)
         #im = Image.frombuffer("RGBA",(s0,s1),buf)
-        c=clientsocket.recv(1024)
-        if not c:
-            break
+        #c=clientsocket.recv(1024)
+        #if not c:
+        #    break
         #s=str(size)
         #i=int(size)
         #s0=int(shape0)
@@ -48,33 +60,42 @@ while 1:
         #im.save("test_images/image4.jpg")
         break
     clientsocket.close()
+    print("closed")
+    server_socket.listen(10)
+    
     (clientsocket, address) = server_socket.accept()
-    while True:
-        l=0
+    print("accepted second connection")
+    print(clientsocket)
+    print(address)
+    
+    l=0
+        
 
-        while l<i:
-            if l==0:
-                buf=clientsocket.recv(i)
-                l+=len(buf)
-            else:
-                rec=clientsocket.recv(i)
-                l+=len(rec)
-                buf+=rec
+    while l<s:
+        if l==0:
+            buf=clientsocket.recv(1024)
+            l+=len(buf)
+        else:
+            rec=clientsocket.recv(1024)
+            l+=len(rec)
+            buf+=rec
         
         #buf=clientsocket.recv(i)
         #print(len(buf))
-        arr=np.frombuffer(buf,dtype="uint8")
+    arr=np.frombuffer(buf,dtype="uint8")
         #print(arr.shape)
-        arr=arr.reshape((s0,s1,3))
+    arr=arr.reshape((shape0,shape1,shape2))
         #print(arr.shape)
         #print(arr)
         
-        im = Image.fromarray(arr)
+
+    im = Image.fromarray(arr)
         #im.show()
-        im_path='test_images/image{}.jpg'.format(idx)
+    im_path='test_images/image{}.jpg'.format(idx)
 
-        im.save('test_images/image{}.jpg'.format(idx))
-
+    im.save('test_images/image{}.jpg'.format(idx))
+    infer_and_match(im_path)
+    clientsocket.close()
     print('finished!!')
     
     
@@ -83,4 +104,3 @@ print("exit while loop")
 
 #print(client_socket.getsockname())
 #client_socket.connect(("", 5005))
-
