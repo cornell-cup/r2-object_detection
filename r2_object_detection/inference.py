c40339839e623b2b5501fc3915018c700adbe6b7
gamma2653 <christopher2653@gmail.com>
2021-04-04 18:44:22 -0400

    Added main guard to inference.py

--- r2_object_detection/inference.py
+++ r2_object_detection/inference.py
@@ -39,7 +39,7 @@ trt_graph = trt.create_inference_graph(
 
 def run_inference_for_single_image(image):
     # Get handles to input and output tensors
-    ops = detection_graph.get_operations()
+    ops = trt_graph.get_operations()
     all_tensor_names = {output.name for op in ops for output in op.outputs}
     tensor_dict = {}
     for tensor_key in [
@@ -48,7 +48,7 @@ def run_inference_for_single_image(image):
     ]:
         tensor_name = tensor_key + ':0'
         if tensor_name in all_tensor_names:
-            tensor_dict[tensor_key] = detection_graph.get_tensor_by_name(
+            tensor_dict[tensor_key] = trt_graph.get_tensor_by_name(
                 tensor_name)
     if 'detection_masks' in tensor_dict:
         # The following processing is only for single image
@@ -65,7 +65,7 @@ def run_inference_for_single_image(image):
         # Follow the convention by adding back the batch dimension
         tensor_dict['detection_masks'] = tf.expand_dims(
             detection_masks_reframed, 0)
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+    image_tensor = trt_graph.get_tensor_by_name('image_tensor:0')
 
     # Run inference
     t1 = time.time()
@@ -90,108 +90,113 @@ def run_inference_for_single_image(image):
     return output_coord, output_classes
 
 
-# Create a pipeline
-pipeline = rs.pipeline()
+def main():
+    # Create a pipeline
+    pipeline = rs.pipeline()
 
-# Create a config and configure the pipeline to stream
-#  different resolutions of color and depth streams
-config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    # Create a config and configure the pipeline to stream
+    #  different resolutions of color and depth streams
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start streaming
-profile = pipeline.start(config)
+    # Start streaming
+    profile = pipeline.start(config)
 
-# Getting the depth sensor's depth scale (see rs-align example for explanation)
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
-print("Depth Scale is: ", depth_scale)
+    # Getting the depth sensor's depth scale (see rs-align example for explanation)
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
+    print("Depth Scale is: ", depth_scale)
 
-# We will be removing the background of objects more than
-#  clipping_distance_in_meters meters away
-clipping_distance_in_meters = 1  # 1 meter
-clipping_distance = clipping_distance_in_meters / depth_scale
+    # We will be removing the background of objects more than
+    #  clipping_distance_in_meters meters away
+    clipping_distance_in_meters = 1  # 1 meter
+    clipping_distance = clipping_distance_in_meters / depth_scale
 
-# Create an align object
-# rs.align allows us to perform alignment of depth frames to others frames
-# The "align_to" is the stream type to which we plan to align depth frames.
-align_to = rs.stream.color
-align = rs.align(align_to)
+    # Create an align object
+    # rs.align allows us to perform alignment of depth frames to others frames
+    # The "align_to" is the stream type to which we plan to align depth frames.
+    align_to = rs.stream.color
+    align = rs.align(align_to)
 
-try:
-    while True:
-        # Get frameset of color and depth
-        frames = pipeline.wait_for_frames()
-        # frames.get_depth_frame() is a 640x360 depth image
+    try:
+        while True:
+            # Get frameset of color and depth
+            frames = pipeline.wait_for_frames()
+            # frames.get_depth_frame() is a 640x360 depth image
 
-        # Align the depth frame to color frame
-        aligned_frames = align.process(frames)
+            # Align the depth frame to color frame
+            aligned_frames = align.process(frames)
 
-        # Get aligned frames
-        aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
-        color_frame = aligned_frames.get_color_frame()
+            # Get aligned frames
+            aligned_depth_frame = aligned_frames.get_depth_frame()  # aligned_depth_frame is a 640x480 depth image
+            color_frame = aligned_frames.get_color_frame()
 
-        # Validate that both frames are valid
-        if not aligned_depth_frame or not color_frame:
-            continue
+            # Validate that both frames are valid
+            if not aligned_depth_frame or not color_frame:
+                continue
 
-        depth_image = np.asanyarray(aligned_depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        print(depth_image)
-        test = cv2.resize(color_image, (1920, 1080))
-        test_depth = cv2.resize(depth_image, (1920, 1080))
-        cv2.imwrite('test.jpg', test)
+            depth_image = np.asanyarray(aligned_depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+            print(depth_image)
+            test = cv2.resize(color_image, (1920, 1080))
+            test_depth = cv2.resize(depth_image, (1920, 1080))
+            cv2.imwrite('test.jpg', test)
 
-        output_coord, output_classes = run_inference_for_single_image(test)
-        try:
+            output_coord, output_classes = run_inference_for_single_image(test)
+            try:
 
-            coordinates = output_coord[output_classes.index("bottle")]
-            x = (coordinates[1] + coordinates[3]) / 2 * 1920
-            y = (coordinates[0] + coordinates[2]) / 2 * 1080
-            inx = test_depth[int(y)][int(x)] * depth_scale
-        except:
-            print('not found')
-            coordinates = [0, 0, 0, 0]
-            x = 0
-            y = 0
-            inx = 0
-        print(output_classes)
-        # print(coordinates)
+                coordinates = output_coord[output_classes.index("bottle")]
+                x = (coordinates[1] + coordinates[3]) / 2 * 1920
+                y = (coordinates[0] + coordinates[2]) / 2 * 1080
+                inx = test_depth[int(y)][int(x)] * depth_scale
+            except:
+                print('not found')
+                coordinates = [0, 0, 0, 0]
+                x = 0
+                y = 0
+                inx = 0
+            print(output_classes)
+            # print(coordinates)
 
-        iny = math.tan(math.radians(43.5)) * inx * 2 * (x - 960) / 1920
-        inz = math.tan(math.radians(29)) * inx * 2 * (y - 540) / 1080
-        print(inx)
-        print(inx, iny, inz)
+            iny = math.tan(math.radians(43.5)) * inx * 2 * (x - 960) / 1920
+            inz = math.tan(math.radians(29)) * inx * 2 * (y - 540) / 1080
+            print(inx)
+            print(inx, iny, inz)
 
-        # Remove background - Set pixels further than clipping_distance to grey
-        grey_color = 153
-        depth_image_3d = np.dstack(
-            (depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
-        bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
-        # Render images
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-        print((coordinates[1] * bg_removed.shape[1], coordinates[0] * bg_removed.shape[0]))
-        cv2.rectangle(color_image,
-                      (int(coordinates[1] * bg_removed.shape[1]), int(coordinates[0] * bg_removed.shape[0])),
-                      (int(coordinates[3] * bg_removed.shape[1]), int(coordinates[2] * bg_removed.shape[0])), 100, 5)
-        print("1")
-        images = np.hstack((depth_colormap, color_image))
-        print("2")
-        cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-        print("3")
-        cv2.imshow('Align Example', color_image)
-        print("4")
-        test2 = cv2.resize(depth_colormap, (1920, 1080))
-        print("5")
-        # if math.sqrt((inx + 0.06985) ** 2 + (iny - 0.10795) ** 2 + (inz - 0.05715) ** 2) < .7:
-        #     kinematics(inx + 0.06985, iny - 0.10795, inz - 0.05715);
-        # cv2.imshow('potato', test2)
-        print("6")
-        key = cv2.waitKey(1)
+            # Remove background - Set pixels further than clipping_distance to grey
+            grey_color = 153
+            depth_image_3d = np.dstack(
+                (depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
+            bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+            # Render images
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            print((coordinates[1] * bg_removed.shape[1], coordinates[0] * bg_removed.shape[0]))
+            cv2.rectangle(color_image,
+                          (int(coordinates[1] * bg_removed.shape[1]), int(coordinates[0] * bg_removed.shape[0])),
+                          (int(coordinates[3] * bg_removed.shape[1]), int(coordinates[2] * bg_removed.shape[0])), 100, 5)
+            print("1")
+            images = np.hstack((depth_colormap, color_image))
+            print("2")
+            cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+            print("3")
+            cv2.imshow('Align Example', color_image)
+            print("4")
+            test2 = cv2.resize(depth_colormap, (1920, 1080))
+            print("5")
+            # if math.sqrt((inx + 0.06985) ** 2 + (iny - 0.10795) ** 2 + (inz - 0.05715) ** 2) < .7:
+            #     kinematics(inx + 0.06985, iny - 0.10795, inz - 0.05715);
+            # cv2.imshow('potato', test2)
+            print("6")
+            key = cv2.waitKey(1)
 
-        # Press esc or 'q' to close the image window
-        if key & 0xFF == ord('q') or key == 27:
-            cv2.destroyAllWindows()
-            break
-finally:
-    pipeline.stop()
+            # Press esc or 'q' to close the image window
+            if key & 0xFF == ord('q') or key == 27:
+                cv2.destroyAllWindows()
+                break
+    finally:
+        pipeline.stop()
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='r2_object_detection',
-    version='1.0',
-    packages=find_packages(where="src"),
-    url='https://cornellcuprobotics.com/',
-    license='',
-    author='Cornell CUP',
-    author_email='',
-    description='r2_object_detection'
+    setup_requires=['pbr'],
+    pbr=True,
 )
