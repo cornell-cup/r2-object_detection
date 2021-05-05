7124d7d44f01ab86e3f4f4b8a06dc2bf78ca8434
Yijia Dai <daiyijia02@gmail.com>
2021-05-05 17:04:21 +0800

    Try read ply file.

---
+++ r2_object_detection/View_point_cloud.py
@@ -0,0 +1,8 @@
+from open3d import *
+
+def main():
+    cloud = read_point_cloud("cloud.ply") # Read the point cloud
+    draw_geometries([cloud]) # Visualize the point cloud
+
+if __name__ == "__main__":
+    main()
