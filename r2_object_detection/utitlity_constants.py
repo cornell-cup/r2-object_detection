5eedaec6237594dfe4c9a23cb5ea1da1f2ddb440
Alison Duan <aduan@Alisons-MacBook-Pro-2.local>
2021-09-09 19:17:46 -0400

    purged old implementation files

--- r2_object_detection/utitlity_constants.py
+++
@@ -1,5 +0,0 @@
-import os
-MODELS_DIR = 'ssd_mobilenet_v1_coco_2018_01_28/'
-PATH_TO_FROZEN_GRAPH = MODELS_DIR + 'frozen_inference_graph.pb'
-PATH_TO_OPTIMIZED_GRAPH = MODELS_DIR + 'optimized_inference_graph.'
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
