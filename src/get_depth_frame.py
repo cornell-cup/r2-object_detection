9d607d716164b2f084e2e6a22453eb63ff8889f9
Iris <icl5@cornell.edu>
2021-10-30 17:32:15 -0400

    Add get_depth_frame.py

---
+++ src/get_depth_frame.py
@@ -0,0 +1,17 @@
+import numpy as np
+from camera import Camera
+with Camera(640,480) as cam:
+  color_frame, depth_frame=cam.get_frames()
+  dist_matrix = np.zeros((480,640))
+  for y in range(480):
+    for x in range(640):
+      dist = depth_frame.get_distance(x, y)
+      dist_matrix[y,x] = dist
+
+  file = open("dist_matrix1.txt", "w+")
+  for rows in dist_matrix:
+    for element in rows:
+      content = str(element)
+      file.write(content + " ")
+    file.write("\n")
+  file.close()
