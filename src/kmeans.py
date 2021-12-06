8bebf99bb7c35212cbdc0818e6589a9a7f7cae60
Iris <icl5@cornell.edu>
2021-12-06 16:46:36 -0500

    Test images

--- src/kmeans.py
+++ src/kmeans.py
@@ -9,12 +9,17 @@ from sklearn import preprocessing
 import os
 from datetime import datetime
 
-def process_features(X):
+def process_features(X, norm_matrix):
     """
     Preprocess features before feeding into algorithm
     """
-    X_trans = preprocessing.normalize(X.T, norm='l2').T
-    return X_trans
+    c = X.shape[-1]
+
+    X = X.reshape((-1, c))
+    #X = preprocessing.normalize(X, norm='l2')
+    X = np.divide(X, norm_matrix) #The last value should have a number between 1 and 10
+    X = X.reshape((480, 640, c))
+    return np.float32(X)
 
 
 
@@ -27,12 +32,9 @@ def sel_features(input_matrix):
     for y in reversed(range(0, len(input_matrix))):
         for x in range (0, len(input_matrix[0])):
             coord_list.append([input_matrix[y,x],x,abs(y-len(input_matrix)+1)])
+    return coord_list
 
 
-
-def visualize(hist):
-    plt.plot(hist)
-    plt.show()
 
 
 def viz_image(images, names):
@@ -41,9 +43,9 @@ def viz_image(images, names):
     """
     for i in range(len(images)):
         cv2.imshow(names[i], images[i])
-    key = cv2.waitKey(0)
+    print("press q to save images, press r to quit, press anything else to load a new image")
     
-    print ("Press q to save image, press anything else to exit")
+    key = cv2.waitKey(0)
     if key & 0xFF == ord('q') or key == 27:
         print ("Saving images")
         now = datetime.now()
@@ -64,18 +66,21 @@ def cv_kmeans(input_matrix):
     """
     
     img = input_matrix
+    #img = sel_features(img)
+    img = process_features(img, [1,1,1,1.5])
     print ("image shape: ", img.shape,"image: ", img)
     c = img.shape[-1]
 
     
     twoDimg = img.reshape((-1,c))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K=10
+    K=5
     attempts = 10
     ret, label, center = cv2.kmeans(twoDimg, K, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
     print ("running kmeans")
     center = np.uint8(center)
     
+    X_trans = preprocessing.normalize
     res = center[label.flatten()]
     result_image = res.reshape((img.shape))
     return result_image
