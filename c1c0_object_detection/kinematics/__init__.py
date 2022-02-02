6f5665d25053774883046f39878c3c416b918554
Alison Duan <aduan@Alisons-MacBook-Pro-2.local>
2022-02-02 17:36:25 -0500

    starting to package the kinematics module

--- c1c0_object_detection/kinematics/__init__.py
+++ c1c0_object_detection/kinematics/__init__.py
@@ -1,1 +1,6 @@
-# from . import test
+from . import collison_detection
+
+# main things we want to be able to access from the kinematics package:
+# 1) Assuming linearity function (should take in a starting angle position and a goal position)
+# 2) Visualization (can be pretty basic to start with)
+# 3) Collision detection functions later? Or at least some way to represent the obstacles after k-means algo
