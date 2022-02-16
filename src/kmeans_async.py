6c69d645fa0d79aff6ecaceb13447c73a8ed79b0
Christopher De Jesus <Christopher2653@gmail.com>
2022-02-16 13:49:28 -0500

    Added kmeans_async tests

--- src/kmeans_async.py
+++ src/kmeans_async.py
@@ -1,28 +1,36 @@
 from multiprocessing import Pool
+from typing import TYPE_CHECKING, Iterable
 
 from kmeans import cv_kmeans, viz_image
+
+
+if TYPE_CHECKING:
+    import numpy
 
 
 DEFAULT_Ks = [5]
 
 
-def find_ks(org_image, rgbd, pool=None, ks=None, workers=4):
+def find_ks(org_image: numpy.ndarray, rgbd: numpy.ndarray, pool: Pool = None,
+            ks: Iterable[int] = None, workers: int = 4, viz : bool = False):
     """
     Visualizes optimal k by running `kmeans.py` on all values of `k` in
     iterable `ks`.
 
     PARAMETERS
     ----------
-    org_image -
+    org_image
         org_image
-    rgbd -
-        A numpy array of red, green, blue, and depth frames.
-    pool -
-        A multiprocessing.Pool pool of workers.
-    ks -
+    rgbd
+        A numpy.ndarray of red, green, blue, and depth frames.
+    pool
+        A multiprocessing.Pool of workers.
+    ks
         A set of iterable Ks to run analysis on.
-    workers -
+    workers
         Number of worker processes to use when analysing the Ks.
+    viz
+        Whether to visualize the images generated
     """
     if pool is None:
         pool = Pool(processes=workers)
@@ -30,8 +38,13 @@ def find_ks(org_image, rgbd, pool=None, ks=None, workers=4):
         ks = DEFAULT_Ks.copy()
     kmean_futures = [pool.apply_async(cv_kmeans, (rgbd, k))
                      for k in ks]
-    for i, kmeans in enumerate(kmean_futures):
-        viz_image([org_image, kmeans.get(), rgbd],
-                  ['Original', f'Result (k={ks[i]})', 'Depth Frame'])
+    kmeans = {ks[i]: kmeans.get() for i, kmeans in enumerate(kmean_futures)}
+    if viz:
+        for i, kmean in enumerate(kmeans):
+            viz_image([org_image, kmean, rgbd],
+                      ['Original', f'Result (k={ks[i]})', 'Depth Frame'])
+    return kmeans
+
+
 
 
---
+++ tests/kmeans_async_test.py
@@ -0,0 +1,53 @@
+import unittest
+import cv2
+import time
+
+from src.kmeans_async import find_ks
+from src.kmeans import cv_kmeans
+
+
+class KmeansAsyncTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.ks = [3, 5, 10]
+        self.org_img = cv2.imread('test_ord_img.jpg')
+        self.rgbd = cv2.imread('test_rgbd.jpg')
+
+    def test_consistency(self):
+        """
+        This test is only meant to test that the results are consistent with
+        non-asynchronous (synchronous) calls.
+        """
+        ks = self.ks
+        org_img = self.org_img
+        rgbd = self.rgbd
+        async_kmeans = find_ks(org_img, rgbd, ks, viz=False)
+        sync_kmeans = {k: cv_kmeans(org_img, k) for k in ks}
+        self.assertEqual(async_kmeans, sync_kmeans, 'Asynchronous kmeans are '
+                                                    'not consistent with '
+                                                    'synchronous kmeans.')
+
+    def test_speed(self):
+        """
+        This test is not the most precise- this is okay. In production, we only
+        want to use async if it is faster. This test gives us a prompt
+        response.
+        """
+        ks = self.ks
+        org_img = self.org_img
+        rgbd = self.rgbd
+        start = time.time()
+        async_kmeans = set(find_ks(org_img, rgbd, ks, viz=False))
+        end = time.time()
+        async_time = end-start
+        start = time.time()
+        sync_kmeans = {cv_kmeans(org_img, k) for k in ks}
+        end = time.time()
+        sync_time = end-start
+        self.assertGreater(sync_time, async_kmeans, 'Asynchronous kmeans took '
+                                                    'longer to generate than'
+                                                    'synchronous kmeans.')
+
+
+if __name__ == '__main__':
+    unittest.main()
