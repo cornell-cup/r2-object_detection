117d3c0dcf97ecaea8f12d0e0588d3b5535c7978
BrandonMan123 <brandonkingyiuman@gmail.com>
2022-02-19 15:45:50 -0500

    Added optimal k

--- src/kmeans_test.py
+++ src/kmeans_test.py
@@ -61,33 +61,51 @@ def viz_image(images, names):
         cv2.destroyAllWindows()
 
 def cv_kmeans(input_matrix):
-    """ Performs kmeans clustering on the input matrix. 
-    Expect the input_matrix to have dimension (k, d, c) where k
-	and d are the image dimension and c is the number of channels in the image
+    """ Performs kmeans clustering on the input matrix. Expect the input_matrix to have dimension (k, d, c) where k
+    and d are the image dimension and c is the number of channels in the image
     """
-    
     img = input_matrix
     #img = sel_features(img)
-    img = process_features(img, [1,1,1,1.5])
-    c = img.shape[-1]
 
-    
+    img = process_features(img, [1,1,1,1.2])
+    print ("image shape: ", img.shape,"image: ", img)
+    c = img.shape[-1]
     twoDimg = img.reshape((-1,c))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K=5
-    attempts = 5
-    ret, label, center = cv2.kmeans(twoDimg, K, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
     print ("running kmeans")
-    center = np.uint8(center)
-    
+    distortions = []
+    labels=[]
+    K_max=5
+    for k in range(1,K_max):
+        attempts = 10
+        ret, label, center = cv2.kmeans(twoDimg, k, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+        labels.append(label)
+        center = np.uint8(center)
+        distortions.append(ret)
+
+    plt.plot(range(1,K_max), distortions, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Distortion')
+    plt.title('The Elbow Method showing the optimal k')
+    plt.show()
     X_trans = preprocessing.normalize
-    res = center[label.flatten()]
+    idx = 0
+    for i in range(len(distortions)-1):
+        if (distortions[i] - distortions[i+1] < (distortions[0]/10)):
+            idx = i
+            break
+    print("k: ", idx+1)
+    res = center[labels[idx].flatten()]
     result_image = res.reshape((img.shape))
+    cv2.imshow("res",result_image)
+    cv2.waitKey()
     return result_image
+
 
 def get_depth_images(dir):
     path = os.path.join(os.getcwd(), "kmeans_test_imgs", dir)
     org_img = image.imread(path+"/Orignal.jpg")
+
     depth_img = cv2.imread(path+"/Depth Frame.jpg",0)
     b, g, r = cv2.split(org_img)
     depth_img = depth_img.astype('float32')
@@ -98,10 +116,8 @@ def get_depth_images(dir):
 
 
 def get_bound (img):
-    print (img)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     vals = np.unique(gray)
-    print (vals)
     for v in vals:
         
         tmp = gray.copy()
@@ -114,8 +130,8 @@ def get_bound (img):
         # erosion takes the min and dilation takes the max
         # since we want to keep white pixels we erode then dilate
         kernel = np.ones((5,5), np.uint8)
-        img_erosion = cv2.erode(tmp, kernel, iterations=2)
-        img_dilation = cv2.dilate(img_erosion, kernel, iterations=2)
+        img_erosion = cv2.erode(tmp, kernel, iterations=4)
+        img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
 
         # Re update the mask using the dilated image where all 
         # desired pixels (white) are set to 1
@@ -134,50 +150,12 @@ def get_bound (img):
     
     cv2.imshow("gray", gray)
 
-"""
-tmp = gray.copy()
-        #create a mask where all values equal to v are 1 and everything else is 0
-        mask = (tmp == v).astype(int) 
-        
-        tmp[mask==0] = 0
-        tmp[mask==1] = 150
-        active_px = np.argwhere(mask!=0)
-        active_px = active_px[:,[1,0]]
-        x,y,w,h = cv2.boundingRect(active_px)
-        # erosion takes the min and dilation takes the max
-        # since we want to keep white pixels we erode then dilate
-        kernel = np.ones((5,5), np.uint8)
-        img_erosion = cv2.erode(tmp, kernel, iterations=2)
-        img_dilation = cv2.dilate(tmp, kernel, iterations=2)
-        
-        cv2.imshow("eroded", img_erosion)
-        cv2.imshow("dilated", img_dilation)
-        cv2.rectangle(tmp,(x,y),(x+w,y+h),(255,0,0),1)
-        cv2.imshow("org", img)
-        cv2.imshow("tmp", tmp)
-"""
-"""
-kernel = np.ones((5,5), np.uint8)
-        img_dilation = cv2.dilate(gray, kernel, iterations=4)
-        img_erosion = cv2.erode(img_dilation, kernel, iterations=1)
-        cv2.imshow("eroded", img_erosion)
-        cv2.imshow("dilated", img_dilation)
 
-        tmp = img_erosion.copy()
-        mask = (tmp == v).astype(int)
-        tmp[mask==0] = 100
-        active_px = np.argwhere(mask!=0)
-        active_px = active_px[:,[1,0]]
-        x,y,w,h = cv2.boundingRect(active_px)
-        cv2.rectangle(tmp,(x,y),(x+w,y+h),(255,0,0),1)
-"""
 
 def main():
-    org_image, depth_img, rgbd = get_depth_images("04-12-13:10:36")
+    org_image, depth_img, rgbd = get_depth_images("04-12-15:49:55")
+    cv2.imshow("org_im", org_image)
     result_img = cv_kmeans(rgbd)
-    #get_bounding_boxes(result_img)
-    # cv2.imshow("Result image", result_img)
-    #get_box(result_img)
     get_bound(result_img)
     
     viz_image([org_image, result_img, depth_img], ["Orignal", "Result", "Depth Frame"])
