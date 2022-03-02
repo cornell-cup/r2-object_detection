fc4ddb73e626a159fa1a548306931b3996bc8fcb
RajBOi30 <45372095+RajBOi30@users.noreply.github.com>
2022-03-02 17:37:39 -0500

    Collision detection fixed with rectangular bounding boxes instead of cubes

--- src/kinematics/collision_detection.py
+++ src/kinematics/collision_detection.py
@@ -1,11 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Button
-from mpl_toolkits.mplot3d import axes3d
-import sys
-import kinpy as kp
+import random
 from rrtnode import RRTNode
-from mpl_toolkits.mplot3d import Axes3D
 
 """****************************************************************************
 A script to configure a robot arm and collisions (represented as cubes).
@@ -46,34 +43,34 @@ def plot_linear_cube(ax, cube):
     ax.plot3D([x + l, x + l], [y, y], [z, z + l], **kwargs)
 
 
-def plot_linear_prism(ax, prism):
+def plot_linear_prism(ax, prism, color):
     """
         Plot a prism on an instance of matplotlib.axes.
 
         INSTANCE ARGUMENTS:
-        ax   [matplotlib.axes] : figure with 3D axes.
+        ax   [matplotlib.axes]  : figure with 3D axes.
         prism [list]            : specifications of the cube formatted as
-                                 [<x coord.>, <y coord.>, <z coord.>, <length.>, <width.>, <height.>].
-
+                                  [<x coord.>, <y coord.>, <z coord.>, <length.>, <width.>, <height.>].
+        color                   : A color for the prism ('red', 'blue', 'green')
         """
     # x, y, z, l = cube
     x, y, z, l, w, h = prism
     # Each item in the arrays "xx" and "yy" represent either an x or y coordinate
-    # For example: (x, y+l), (x, y), (x+w, y), ... etc
+    # For example: (x, y+w), (x, y), (x+l, y), ... etc
     # The order of the coordinates determine which dot is plotted first, and the lines
     # between each are drawn in the same order.
-    # Also, the l and w are used as the area of the base of the prism, with the length
-    # moving in the positive y direction, width in the positive x, and height is z obv.
-    xx = [x, x, x + w, x + w, x]
-    yy = [y + l, y, y, y + l, y + l]
+    # Also, the l and w are used as the area of the base of the prism, with the width
+    # moving in the positive y direction, length in the positive x, and height is z obv.
+    xx = [x, x, x + l, x + l, x]
+    yy = [y + w, y, y, y + w, y + w]
 
-    kwargs = {'alpha': 1, 'color': 'blue'}
+    kwargs = {'alpha': 1, 'color': color}
     ax.plot3D(xx, yy, [z] * 5, **kwargs)
     ax.plot3D(xx, yy, [z + h] * 5, **kwargs)
     ax.plot3D([x, x], [y, y], [z, z + h], **kwargs)
-    ax.plot3D([x, x], [y + l, y + l], [z, z + h], **kwargs)
-    ax.plot3D([x + w, x + w], [y + l, y + l], [z, z + h], **kwargs)
-    ax.plot3D([x + w, x + w], [y, y], [z, z + h], **kwargs)
+    ax.plot3D([x, x], [y + w, y + w], [z, z + h], **kwargs)
+    ax.plot3D([x + l, x + l], [y + w, y + w], [z, z + h], **kwargs)
+    ax.plot3D([x + l, x + l], [y, y], [z, z + h], **kwargs)
 
 
 def plot_points(ax, xs, ys, zs):
@@ -117,8 +114,6 @@ def plot_arm(ax, node, line_label):
     line_label [str]             : label for the arm plot.
     """
     points = node.joint_positions
-    for p in points:
-        print("point", p[0], " ", p[1], " ", p[2])
     plot_points(ax, [p[0] for p in points],
                 [p[1] for p in points], [p[2] for p in points])
     plot_lines(ax, [p[0] for p in points],
@@ -229,42 +224,8 @@ def arm_is_colliding_prism(node, prism):
     return False
 
 
-def main():
-    fig = plt.figure()
-    ax = plt.axes(projection="3d")
-    # Set the limits for the range of plotted values
-    lim = .6
-    plt.xlim(-lim, lim)
-    plt.ylim(-lim, lim)
-    ax.set_zlim(-lim, lim)
-    # create the prism to be avoided (hard coded)
-    prism = [0, .1, 0, .1, .2, .4]
-    plot_linear_prism(ax, prism)
-    # create the arm and report collision (random)
-    arm = RRTNode(None)
-    collision_detected = arm_is_colliding_prism(arm, prism)
-    print("Arm is colliding:", collision_detected)
-    plot_arm(ax, arm, "arm")
-    if collision_detected:
-        plt.title("Collision Detected")
-    else:
-        plt.title("No Collision Detected")
-
-    class Index:
-        def draw(self, event):
-            plt.close()
-            main()
-
-    axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
-    run = Button(axnext, 'ReRun')
-    callback = Index()
-    run.on_clicked(callback.draw)
-    plt.legend()
-
-    plt.show()
-
 def newLineCollider(line_seg, prism):
-        """
+    """
         Return True if the line segment is colliding with the prism, else False.
 
         INSTANCE ARGUMENTS:
@@ -282,124 +243,203 @@ def newLineCollider(line_seg, prism):
         t = (x-x1) / (x2-x1)
 
         """
-        collision = False
-        checkX = False
-        checkY = False
-        checkZ = False
-        # First check if either point is within the prism:
-        if (prism[0] <= line_seg[0]) & (line_seg[0] <= prism[0] + prism[4]):
-            checkX = True
-        if (prism[1] <= line_seg[1]) & (line_seg[1] <= prism[1] + prism[3]):
-            checkY= True
-        if (prism[2] <= line_seg[2]) & (line_seg[2] <= prism[2] + prism[5]):
-            checkZ = True
-        if(checkX & checkZ & checkY):
+    collision = False
+    checkX = False
+    checkY = False
+    checkZ = False
+    # First check if either point is within the prism: (x,y,z)
+    if (prism[0] <= line_seg[0]) & (line_seg[0] <= prism[0] + prism[3]):
+        checkX = True
+    if (prism[1] <= line_seg[1]) & (line_seg[1] <= prism[1] + prism[4]):
+        checkY = True
+    if (prism[2] <= line_seg[2]) & (line_seg[2] <= prism[2] + prism[5]):
+        checkZ = True
+    if (checkX & checkZ & checkY):
+        return True
+    checkX = False
+    checkY = False
+    checkZ = False
+    if (prism[0] <= line_seg[3]) & (line_seg[3] <= prism[0] + prism[3]):
+        checkX = True
+    if (prism[1] <= line_seg[4]) & (line_seg[4] <= prism[1] + prism[4]):
+        checkY = True
+    if (prism[2] <= line_seg[5]) & (line_seg[5] <= prism[2] + prism[5]):
+        checkZ = True
+    if (checkX & checkZ & checkY):
+        print('POINT TYPE COLLISION')
+        return True
+
+    checkX = False
+    checkY = False
+    checkZ = False
+
+    # create some constants
+    x = prism[0]
+    y = prism[1]
+    z = prism[2]
+    print('x', x)
+    print('y', y)
+    print('z', z)
+    l = prism[3]  # length goes with x
+    w = prism[4]  # width goes with y
+    h = prism[5]
+    x1 = line_seg[0]
+    x2 = line_seg[3]
+    y1 = line_seg[1]
+    y2 = line_seg[4]
+    z1 = line_seg[2]
+    z2 = line_seg[5]
+
+    print("p1x: ", x1)
+    print("p1y: ", y1)
+    print("p1z: ", z1)
+    print("p2x: ", x2)
+    print("p2y: ", y2)
+    print("p2z: ", z2)
+
+    # check if the line collides with either  x surface
+    if (x2 - x1 != 0):
+        t1 = (x - x1) / (x2 - x1)
+        t2 = ((x + l) - x1) / (x2 - x1)
+        print("t1", t1)
+        print("t2", t2)
+        yt1 = y1 + t1 * (y2 - y1)
+        yt2 = y1 + t2 * (y2 - y1)
+        zt1 = z1 + t1 * (z2 - z1)
+        zt2 = z1 + t2 * (z2 - z1)
+        xt1 = x1 + t1 * (x2 - x1)
+        xt2 = x1 + t2 * (x2 - x1)
+
+        print('xt1:', xt1)
+        print('xt2:', xt2)
+        print('yt1:', yt1)
+        print('yt2:', yt2)
+        print('zt1:', zt1)
+        print('zt2:', zt2)
+        if (y <= yt1 and yt1 <= y + w and z <= zt1 and zt1 <= z + h and t1 >= 0 and t1 <= 1):
+            print('collision first side X')
             return True
-        checkX = False
-        checkY = False
-        checkZ = False
-        if (prism[0] <= line_seg[3]) & (line_seg[3] <= prism[0] + prism[4]):
-            checkX = True
-        if (prism[1] <= line_seg[4]) & (line_seg[4] <= prism[1] + prism[3]):
-            checkY = True
-        if (prism[2] <= line_seg[5]) & (line_seg[5] <= prism[2] + prism[5]):
-            checkZ = True
-        if(checkX & checkZ & checkY):
+        elif (y <= yt2 and yt2 <= y + w and z <= zt2 and zt2 <= z + h and t2 >= 0 and t2 <= 1):
+            print('collision second side X')
             return True
+    print('no collision in x planes')
 
-        checkX = False
-        checkY = False
-        checkZ = False
+    # check if the line collides with either y surface
+    if (y2 - y1 != 0):
+        t1 = (y - y1) / (y2 - y1)
+        t2 = ((y + w) - y1) / (y2 - y1)
+        xt1 = x1 + t1 * (x2 - x1)
+        xt2 = x1 + t2 * (x2 - x1)
+        zt1 = z1 + t1 * (z2 - z1)
+        zt2 = z1 + t2 * (z2 - z1)
+        if (x <= xt1 and xt1 <= x + l and z <= zt1 and zt1 <= z + h and t1 >= 0 and t1 <= 1):
+            print('collision first side Y')
+            print(xt1)
+            print(zt1)
+            return True
+        elif (x <= xt2 and xt2 <= x + l and z <= zt2 and zt2 <= z + h and t2 >= 0 and t2 <= 1):
+            print('collision second side Y')
+            print(xt2)
+            print(zt2)
+            return True
+    print('no collision in y planes')
 
-        # check if the line collides with the x surface
-        if(line_seg[3] - line_seg[0] != 0):
-            tx = (prism[0] - line_seg[0]) / (line_seg[3] - line_seg[0])
-            print("T for X:", tx)
-            yt = line_seg[1] + tx * (line_seg[4] - line_seg[1])
-            zt = line_seg[2] + tx * (line_seg[5] - line_seg[2])
-            if(prism[1] <= yt) and (yt <= prism[1] + prism[3]):
-                if(prism[2] <= zt) and (zt <= prism[2] + prism[5]):
-                    return True
-        else:
-            if(prism[0] <= line_seg[0]) & (line_seg[0] <= prism[0] + prism[4]):
-                checkX = True
+    # check if the line collides with either z surface
+    if (z2 - z1 != 0):
+        t1 = (z - z1) / (z2 - z1)
+        t2 = ((z + h) - z1) / (z2 - z1)
+        xt1 = x1 + t1 * (x2 - x1)
+        xt2 = x1 + t2 * (x2 - x1)
+        yt1 = y1 + t1 * (y2 - y1)
+        yt2 = y1 + t2 * (y2 - y1)
 
-        # check if line collides with y surface
-        if (line_seg[4] - line_seg[1] != 0):
-            ty = (prism[1] - line_seg[1]) / (line_seg[4] - line_seg[1])
-            print("T for Y:", ty)
-            zt = line_seg[2] + ty * (line_seg[5] - line_seg[2])
-            xt = line_seg[0] + ty * (line_seg[3] - line_seg[0])
-            if (prism[2] <= zt) and (zt <= prism[2] + prism[5]):
-                if (prism[0] <= xt) & (xt <= prism[0] + prism[4]):
-                    return True
-        else:
-            if (prism[1] <= line_seg[1]) and (line_seg[1] <= prism[1] + prism[3]):
-                checkY = True
-
-        # check if line collides with z surface
-        if (line_seg[5] - line_seg[2] != 0):
-            tz = (prism[2] - line_seg[2]) / (line_seg[5] - line_seg[2])
-            print("T for Z:", tz)
-            yt = line_seg[1] + tz * (line_seg[4] - line_seg[1])
-            xt = line_seg[0] + tz * (line_seg[3] - line_seg[0])
-            if (prism[1] <= yt) and (yt <= prism[2] + prism[3]):
-                if (prism[0] <= xt) & (xt <= prism[0] + prism[4]):
-                    return True
-        else:
-            if (prism[1] <= line_seg[1]) and (line_seg[1] <= prism[1] + prism[3]):
-                checkY = True
-        return False
+        if (x <= xt1 and xt1 <= x + l and y <= yt1 and yt1 <= y + w and t1 >= 0 and t1 <= 1):
+            print('collision first side Z')
+            return True
+        elif (x <= xt2 and xt2 <= x + l and y <= yt2 and yt2 <= y + w and t2 >= 0 and t2 <= 1):
+            print('collision second side Z')
+            return True
+    print('no collision in z planes')
 
 
-if __name__ == "__main__":
+def checkCollision(prism, rrtNode):
+    collision = False
+    for i in range(len(rrtNode.joint_positions) - 1):
+        p1 = ([rrtNode.joint_positions[i]])
+        p2 = ([rrtNode.joint_positions[i + 1]])
+        # print([p1[0][0],p1[0][1],p1[0][2]])
+        line_seg = ([p1[0][0], p1[0][1], p1[0][2], p2[0][0], p2[0][1], p2[0][2]])
+        if (newLineCollider(line_seg, prism)):
+            print('Collision on line', i, '-', i + 1)
+            print("p1", p1)
+            print("p2", p2)
+            return True
+    return False
+
+
+def main():
+    """
+    Places a prism randomly in 3D space, and draws a larger prism around it that represents it's
+    bounding box. Then checks if a randomly generated arm collides the outer prism at any point.
+
+    """
     fig = plt.figure()
     ax = plt.axes(projection="3d")
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
     # Set the limits for the range of plotted values
     lim = .6
     plt.xlim(-lim, lim)
     plt.ylim(-lim, lim)
     ax.set_zlim(-lim, lim)
     # create the prism to be avoided (hard coded)
-    prism = ([0, 0, 0, .2, .3, .4])
-    plot_linear_prism(ax, prism)
-    # create the arm and report collision (random)
-    line_seg = ([.1, .1, -.3,
-                .1, .1, .5]) #([-.1, .1, .1, .3, .1, .1])
-    xs = ([line_seg[0], line_seg[3]])
-    ys = ([line_seg[1], line_seg[4]])
-    zs = ([line_seg[2], line_seg[5]])
-    print("X1", line_seg[0])
-    print("X2", line_seg[3])
-    print("X Range: ", prism[0], "->", prism[4])
-    plot_lines(ax, xs,ys,zs,None)
-    plt.title(newLineCollider(line_seg,prism))
+    lim = .3
+    location = random.uniform(-lim,lim)
+    prism = ([location, location, location, .2, .3, .4])
+    exoPrism = ([prism[0]-.05,prism[1]-.05,prism[2]-.05,prism[3]+.1,prism[4]+.1,prism[5]+.1])
+    plot_linear_prism(ax, prism, 'blue')
+    plot_linear_prism(ax, exoPrism, 'red')
+    lim = .5
+    """
+    This code was used to generate a single line, and has since been replaced 
+    by code that generates a random N link arm. 
+    
+        randomX1 = random.uniform(-lim, lim)
+        randomY1 = random.uniform(-lim, lim)
+        randomZ1 = random.uniform(-lim, lim)
+    
+        randomX2 = random.uniform(-lim, lim)
+        randomY2 = random.uniform(-lim, lim)
+        randomZ2 = random.uniform(-lim, lim)
+        line_seg = ([randomX1, randomY1, randomZ1,
+                     randomX2, randomY2, randomZ2])  # ([-.1, .1, .1, .3, .1, .1])
+        # line_seg = ([-.3,.15,-.2,.6,.15,.2]) # intersects x faces
+        # line_seg = ([.16, -.3, .2, .16, .3, .2]) # intersects y faces
+        # line_seg = ([.2, -.2, -.3, .2, .15, .5]) # intersects z faces
+        xs = ([line_seg[0], line_seg[3]])
+        ys = ([line_seg[1], line_seg[4]])
+        zs = ([line_seg[2], line_seg[5]])
+        plot_lines(ax, xs, ys, zs, None)
+    """
+    # Create a random arm
+    arm = RRTNode(None)
+    plot_arm(ax,arm,None)
+    class Index:
+        def draw(self, event):
+            plt.close()
+            main()
+
+    axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
+    run = Button(axnext, 'ReRun')
+    callback = Index()
+    run.on_clicked(callback.draw)
+    if checkCollision(exoPrism,arm):
+        plt.title("Collision Detected!")
+    else:
+        plt.title("Wow no Collision")
     plt.show()
-    # main()
-
-"""
-    Strategy for 
-"""
 
 
-
-
-"""
-take code for web use: 
-
-
-import numpy as np
-
-#line_seg - create fake one
-cube = ([11,22,33,1])
-
-a_vec = np.array([line_seg[0][0], line_seg[0][1], line_seg[0][2]])
-b_vec = np.array([line_seg[1][0], line_seg[1][1], line_seg[1][2]])
-side = cube[3]
-c_vec = np.array([cube[0], cube[1], cube[2]]) + side/2
-r = np.sqrt(3) * side / 2
-line_vec = b_vec - a_vec
-line_mag = np.linalg.norm(line_vec)
-circle_vec = c_vec - a_vec
-proj = circle_vec.dot(line_vec / line_mag)
-"""
+if __name__ == "__main__":
+    main()
