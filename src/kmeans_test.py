e7a6d1eba1d51be4e43a58200cdc3ac8717e7351
BrandonMan123 <brandonkingyiuman@gmail.com>
2022-03-14 17:15:27 -0400

    Added debug flag

--- src/kmeans_test.py
+++ src/kmeans_test.py
@@ -16,7 +16,6 @@ def process_features(X, norm_matrix):
     c = X.shape[-1]
 
     X = X.reshape((-1, c))
-    #X = preprocessing.normalize(X, norm='l2')
     X = np.divide(X, norm_matrix) #The last value should have a number between 1 and 10
     X = X.reshape((480, 640, c))
     return np.float32(X)
@@ -26,7 +25,6 @@ def process_features(X, norm_matrix):
 def sel_features(input_matrix):
     """
     Selects the desired features in a depth frame
-    TODO: Reformat the depth frame or reshape the output
     """
     coord_list = []
     for y in reversed(range(0, len(input_matrix))):
@@ -35,57 +33,41 @@ def sel_features(input_matrix):
     return coord_list
 
 
-def preprocess_im(depth_image, color_matrix):
-    """ Apply a mask on the depth map. Basically you look at the pixels
-    that have a high depth value then color those parts super white"""
+def preprocess_data(depth_image, color_matrix, rgbd):
+    rgbd = process_features(rgbd, [1,1,1,1.])
+    return rgbd
     
-    # max_depth = np.min(depth_image) #get max depth value of scene
-    # pct_depth = 0.9
-    # thresh = 110 # min(max_depth * (1+pct_depth)+10, 50) #set thresh, check if min or max
-
-    # mask = (depth_image > thresh).astype(int) # mask is 0 if depth val > thresh
-    # color_matrix[mask==0] = [255, 255, 255]
-    # gray = cv2.cvtColor(color_matrix, cv2.COLOR_BGR2GRAY)
-    # thresh = cv2.threshold(gray, 11, 255, cv2.THRESH_BINARY)[1]
-    # cv2.imshow("gray", gray)
-    # cv2.imshow("depth",depth_image)
-    # cv2.imshow("color",color_matrix)
-
-    return color_matrix
-    
-def unique_count_app(a):
-    colors, count = np.unique(a.reshape(-1,a.shape[-1]), axis=0, return_counts=True)
-    return colors[count.argmax()]
 
 def postprocess_im(depth_img, segmentation, labelled_img):
     """Identify points that violate the depth image and identify the 
-    labels of the pixels. If >50% of pixels of that label violate the depth 
+    labels of the pixels. If >prop% of pixels of that label violate the depth 
     threshold then remove it from the image """
-
-
     """
     for each label:
         get all pixels associated with label. Call this set P
         for each pixel p in P, check if p's depth value is smaller than the threshold
-        if the number of violating pixels in P is greater than 50% of |P|, white out the segmentation
-     """
+        if the number of violating pixels in P is greater than prop% of |P|,
+        white out the segmentation
+    """
 
-    thresh = 50
+    # Fixed threshold either has 125 or 100 refer to 04-12-14:49:33 and 04-12-13:10:36
+    thresh = max(np.min(depth_img), 100) # set threshold
     labels = np.unique(labelled_img)
     labelled_img = labelled_img.reshape(depth_img.shape)
+    prop = 0.3
+    
     for label in labels:
-        coords = np.where(labelled_img == label)
+        coords = np.count_nonzero(labelled_img == label)
+        # a pixel violates if it's part of P and depth value is less than thresh
         violated_px = labelled_img[(labelled_img==label) & (depth_img < thresh)]
-        if len(violated_px) > 0.5 * len(labelled_img):
-            print ("label ", label,"violated")
+        if len(violated_px) > prop * coords:
+            print ("label", label,"violated")
             segmentation[labelled_img==label] = [255,255,255,255]
 
     cv2.imshow('new segmentation', segmentation)
     cv2.waitKey()
-    # print(segmentation)
-    # print(segmentation[segmentation==common_label][0])
-    #cv2.imshow('seg', segmentation)
-    #cv2.waitKey()
+    return segmentation
+
 
 
 def viz_image(images, names):
@@ -111,14 +93,12 @@ def viz_image(images, names):
     if key & 0xFF == ord('q'):
         cv2.destroyAllWindows()
 
-def cv_kmeans(input_matrix):
-    """ Performs kmeans clustering on the input matrix. Expect the input_matrix to have dimension (k, d, c) where k
-    and d are the image dimension and c is the number of channels in the image
+def cv_kmeans(img, debug=False):
+    """ 
+    Performs kmeans clustering on the input matrix. Expect the input_matrix 
+    to have dimension (k, d, c) where k and d are the image dimension and c is 
+    the number of channels in the image
     """
-    img = input_matrix
-    #img = sel_features(img)
-
-    img = process_features(img, [1,1,1,1.5])
     c = img.shape[-1]
     twoDimg = img.reshape((-1,c))
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
@@ -128,16 +108,18 @@ def cv_kmeans(input_matrix):
     K_max=6
     for k in range(1,K_max):
         attempts = 10
-        ret, label, center = cv2.kmeans(twoDimg, k, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+        ret, label, center = cv2.kmeans(twoDimg, k, None, criteria, attempts, 
+        cv2.KMEANS_PP_CENTERS)
         labels.append(label)
         center = np.uint8(center)
         distortions.append(ret)
 
-    plt.plot(range(1,K_max), distortions, 'bx-')
-    plt.xlabel('k')
-    plt.ylabel('Distortion')
-    plt.title('The Elbow Method showing the optimal k')
-    plt.show()
+    if debug:
+        plt.plot(range(1,K_max), distortions, 'bx-')
+        plt.xlabel('k')
+        plt.ylabel('Distortion')
+        plt.title('The Elbow Method showing the optimal k')
+        plt.show()
     X_trans = preprocessing.normalize
     idx = 0
     for i in range(len(distortions)-1):
@@ -145,9 +127,11 @@ def cv_kmeans(input_matrix):
             idx = i
             break
     print("k: ", idx+1)
-    # why is it 4?
+
     res = center[labels[idx].flatten()]
     result_image = res.reshape((img.shape))
+
+    cv2.imshow("res", result_image)
     return result_image, labels[idx]
 
 
@@ -168,10 +152,9 @@ def create_rgbd(rgb_img, depth_img):
     
     return rgbd
 
-def get_bound (img):
+def get_bound (img, debug=False):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     vals = np.unique(gray)
-    print (vals)
     for v in vals:
         if v == 255:
             continue
@@ -184,7 +167,7 @@ def get_bound (img):
         
         # erosion takes the min and dilation takes the max
         # since we want to keep white pixels we erode then dilate
-        kernel = np.ones((5,5), np.uint8)
+        kernel = np.ones((7,7), np.uint8)
         img_erosion = cv2.erode(tmp, kernel, iterations=4)
         img_dilation = cv2.dilate(img_erosion, kernel, iterations=1)
 
@@ -195,8 +178,9 @@ def get_bound (img):
         active_px = active_px[:,[1,0]]
         x,y,w,h = cv2.boundingRect(active_px)
         
-        cv2.imshow("eroded", img_erosion)
-        cv2.imshow("dilated", img_dilation)
+        if debug:
+            cv2.imshow("eroded", img_erosion)
+            cv2.imshow("dilated", img_dilation)
         cv2.rectangle(tmp,(x,y),(x+w,y+h),(255,0,0),1)
         cv2.imshow("org", img)
         cv2.imshow("tmp", tmp)
@@ -209,17 +193,14 @@ def get_bound (img):
 
 
 def main():
-    org_img, depth_img = get_depth_images("04-12-14:49:33")
-    rgb_img = preprocess_im(depth_img, org_img)
-    rgbd = create_rgbd(rgb_img, depth_img)
-    
+    org_img, depth_img = get_depth_images("04-12-14:51:37")
+    rgbd = create_rgbd(org_img, depth_img)
+    cv2.imshow('org',org_img)
 
-
+    preprocessed_rgbd = preprocess_data(depth_img, org_img, rgbd)
     result_img, labels = cv_kmeans(rgbd)
-    postprocess_im(depth_img, result_img, labels)
-    
-
-    get_bound(result_img)
+    result_img = postprocess_im(depth_img, result_img, labels)
+    get_bound(result_img, True)
     
     viz_image([org_img, result_img, depth_img], ["Orignal", "Result", "Depth Frame"])
 
@@ -227,4 +208,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
