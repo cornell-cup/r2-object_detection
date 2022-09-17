b8ce508358d1e93015f84d36471f642bcb0b1ea2
Alison Duan <aduan@Alisons-MacBook-Pro-3.local>
2022-09-17 13:37:54 -0400

    added times for each stage

--- inf_od_nwk.py
+++ inf_od_nwk.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import cv2
 import numpy as np
+import time
 
 from c1c0_object_detection.object_detection.camera import Camera
 from c1c0_object_detection.object_detection.inference import Inference
@@ -16,12 +17,19 @@ WIDTH = 640
 HEIGHT = 480
 DISPLAY = False
 
+def print_time(msg, start):
+    print(msg, time.time()-start," s")
+    return time.time()
+
+
 def main():
+    start_time = time.time()
     with Camera(WIDTH, HEIGHT) as cam:
         # TODO: Do we need context managers for these also?
         inf = Inference()
         grasping = Grasping()
 
+        start_time = print_time("Loaded Cam, Inf, and Grasp Modules: ", start_time)
         for i in range(5):
             # --------- Get Frames and Numpy Images ---------
             try:
@@ -48,6 +56,7 @@ def main():
             bbox = (max(0, left-20), min(WIDTH-1, right+20),
             max(0, top), min(HEIGHT-1, bot))
             
+            start_time = print_time("Detections: ", start_time)
             # --------- Locate where to Grab the Target Object ---------
             isReachable, isGrabbable, coord1, coord2 = grasping.locate_object(
                 dgr, bbox, depth_frame, display=DISPLAY)
@@ -59,6 +68,7 @@ def main():
                 continue
             print("Grasp coordinates in meters (X, Y, Z): ", coord1, coord2)
 
+            start_time = print_time("Calculated Grasps: ", start)
             key = cv2.waitKey(0) # display results
             # TODO: should this be moved after all the rest of the code?
             if key & 0xFF == ord('q') or key == 27:
@@ -67,6 +77,7 @@ def main():
             if key & 0xFF == ord('r'):
                 cv2.destroyAllWindows()
 
+            start_time = print_time("Displayed Grasps: ", start_time)
             # Identify Obstacles?
 
             # - Send Grasp Coordinates to Base Station to compute Arm Configs -
@@ -75,22 +86,24 @@ def main():
 
             # --------- Send Arm Configs to the Arm to move ---------
             arm.init_serial()
-            print("serial port initialized")
             startpos = arm.read_encoder_values()
-            print("arm vals read")
             # inverse kinematics
             avg = [(coord1[i][0] + coord2[i][0])/2
                           for i in range(len(gripper_pt1_arm))]
-            print("target calculated", avg)
+            start_time = print_time("Read Encoder Values: ", start_time)
+
             arm_config, success = alr.linear_rrt_to_point(startpos, avg[0], avg[1], avg[2], [], 1000)
+            start_time = print_time("Calculated Kinematics: ", start_time)
             # send arm_config to the arm to move
             if success:
                 for config in arm_config:
                     converted_array = alr.radians_to_degrees(config)
                     print("WRITING ARM CONFIG", converted_array)
                     arm.publish_updates(converted_array, 0.5)
-            print("arm config serial written")
+            # print("arm config serial written")
             arm.close_serial()
+            start_time = print_time("Arm Finished Moving: ", start_time)
+            break
 
 if __name__ == '__main__':
     main()
