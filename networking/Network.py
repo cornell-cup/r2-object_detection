047ed50c4bfe3e6fcb4a501aa8d640d0a00a53fe
Alison Duan <aduan@Alisons-MacBook-Pro-3.local>
2022-10-16 23:15:37 -0400

    removed sock stream from test socket

--- networking/Network.py
+++ networking/Network.py
@@ -15,7 +15,7 @@ class Network(object):
 
     @staticmethod
     def get_ip() -> str:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
             # doesn't even have to be reachable
             s.connect(('10.255.255.255', 1))
